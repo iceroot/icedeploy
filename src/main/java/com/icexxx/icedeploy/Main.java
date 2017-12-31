e67ebf3b43a712c0a998cd77b41aed7fd0ce600f
iceroot <zxcyhn@126.com>
2017-12-31 09:46:55 +0800

    edit config file

--- src/main/java/com/icexxx/icedeploy/Main.java
+++ src/main/java/com/icexxx/icedeploy/Main.java
@@ -3,16 +3,16 @@ package com.icexxx.icedeploy;
 public class Main {
 
 	public static void main(String[] args) {
-		String host = "118.190.205.147";
+		String host = "192.168.1.123";
 		String username = "root";
-		String password = "Hutool12";
+		String password = "root";
 		int port = 22;
-		String localFileName = "C:/Users/Administrator/Desktop/tp/ssm.war";
+		String localFileName = "C:/Users/Administrator/Desktop/web.war";
 		String remoteTomcat = "/root/apache-tomcat-7.0.82";
 		String remoteTemp = "/root/temp";
 		String remoteBak = "/root/bak";
 		String jdkHome = "/usr/lib/jvm/jdk1.8.0_151";
-		String url = "http://118.190.205.147:8080/ssm/s";
+		String url = "http://192.168.1.123:8080/web";
 		DeployCheck deployCheck = null;
 		DeployService.deploy(host, username, password, port, localFileName, remoteTomcat, remoteTemp, remoteBak,
 				jdkHome, url, deployCheck);
